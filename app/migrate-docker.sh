7d0167e0114ece0f1035e9f5ec3bc534f183fae3
alta7700 <alta700@mail.ru>
2022-12-02 21:05:04 +0300

    push server

--- app/migrate-docker.sh
+++ app/migrate-docker.sh
@@ -1,4 +1,4 @@
 # don`t run makemigrations in docker image
 export MIGRATION_ALERT=1
-python manage.py migrate
+python manage.py migrate && python manage.py collectstatic --noinput
 unset MIGRATION_ALERT
